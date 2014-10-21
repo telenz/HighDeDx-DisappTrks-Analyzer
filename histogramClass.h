e2eca8405e62b9500b614c3510f1b20718a5ced9
Teresa Lenz <teresa.lenz@desy.de>
2014-10-21 15:17:42 +0200

    added come histogram + change the range

--- histogramClass.h
+++ histogramClass.h
@@ -19,8 +19,8 @@ class Hist
   TH1D *htrackPt;
   TH1D *htrackP;
   TH1D *htrackEta;
-  //TH1D *htrackd0;
-  //TH1D *htrackdz;
+  TH1D *htrackd0;
+  TH1D *htrackdz;
   TH1D *htrackNValid;
   TH1D *htrackNLostMid;
   TH1D *htrackNLostInner;
@@ -28,9 +28,15 @@ class Hist
   TH1D *htrackIsolation;
   TH1D *htrackCaloIsolation;
   TH1D *htrackASmi;
+  TH1D *htrackASmi_3;
+  TH1D *htrackASmi_7;
   TH1D *htrackASmiNP;
+  TH1D *htrackASmiNP_3;
+  TH1D *htrackASmiNP_7;
   TH1D *htrackDeDxHarm2;
+  TH1D *htrackHighPurity;
   TH2D *htrackPtDeDxHarm2;
+  TH2D *htrackPtASmi;
   TH2D *htrackPtCaloIso;
   TH2D *htrackPtNLostOuter;
 
@@ -74,18 +80,24 @@ class Hist
       htrackPt = new TH1D("htrackPt","htrackPt",100,0,2000);
       htrackP = new TH1D("htrackP","htrackP",100,0,2000);
       htrackEta = new TH1D("htrackEta","htrackEta",100,-5,5);
-      //htrackd0 = new TH1D("htrackd0","htrackd0",100,-5,5);
-      //htrackdz = new TH1D("htrackdz","htrackdz",100,-50,50);
+      htrackd0 = new TH1D("htrackd0","htrackd0",100,-5,5);
+      htrackdz = new TH1D("htrackdz","htrackdz",100,-50,50);
       htrackNValid = new TH1D("htrackNValid","htrackNValid",40,0,40);
       htrackNLostMid = new TH1D("htrackNLostMid","htrackNLostMid",10,0,10);
       htrackNLostInner = new TH1D("htrackNLostInner","htrackNLostInner",20,0,20);
       htrackNLostOuter = new TH1D("htrackNLostOuter","htrackNLostOuter",20,0,20);
       //hRelativePtError = new TH1D("hRelativePtError" ,"hRelativePtError",300,0,3);
       htrackIsolation = new TH1D("htrackIsolation" ,"htrackIsolation",1000,0,50);
-      htrackCaloIsolation = new TH1D("htrackCaloIsolation" ,"htrackCaloIsolation",1000,0,3000);
-      htrackDeDxHarm2 = new TH1D("htrackDeDxHarm2" ,"htrackDeDxHarm2",100,0,50);
-      htrackASmi = new TH1D("htrackASmi" ,"htrackASmi",1000,-1.1,1.1);
-      htrackASmiNP = new TH1D("htrackASmiNP" ,"htrackASmiNP",1000,-1.1,1.1);
+      htrackCaloIsolation = new TH1D("htrackCaloIsolation" ,"htrackCaloIsolation",300,0,3000);
+
+      htrackDeDxHarm2  = new TH1D("htrackDeDxHarm2" ,"htrackDeDxHarm2",100,0,50);
+      htrackASmi       = new TH1D("htrackASmi" ,"htrackASmi",50,-1.1,1.1);
+      htrackASmi_3     = new TH1D("htrackASmi_3" ,"htrackASmi_3",50,-1.1,1.1);
+      htrackASmi_7     = new TH1D("htrackASmi_7" ,"htrackASmi_7",50,-1.1,1.1);
+      htrackASmiNP     = new TH1D("htrackASmiNP" ,"htrackASmiNP",50,-1.1,1.1);
+      htrackASmiNP_3   = new TH1D("htrackASmiNP_3" ,"htrackASmiNP_3",50,-1.1,1.1);
+      htrackASmiNP_7   = new TH1D("htrackASmiNP_7" ,"htrackASmiNP_7",50,-1.1,1.1);
+      htrackHighPurity = new TH1D("htrackHighPurity","htrackHighPurity",2,0,2);
 
       htrackPdgId = new TH1D("htrackPdgId","htrackPdgId",500,0,500);
       htrackgenParticle = new TH1D("htrackgenParticle","htrackgenParticle",1,0,1);
@@ -115,15 +127,17 @@ class Hist
       //hMass_3Hits = new TH1D("hMass_3Hits","hMass_3Hits",75,0,1500);
       //hMass_1Hits = new TH1D("hMass_1Hits","hMass_1Hits",75,0,1500);
 
-      hnPFJetsub    = new TH1D("hnPFJetsub","hnPFJetsub",20,0,20);
-      hDeltaPhi     = new TH1D("hDeltaPhi","hDeltaPhi",32,0,3.2);
-      hDeltaPhiMax  = new TH1D("hDeltaPhiMax","hDeltaPhiMax",32,0,3.2);
-      h1stjetpt     = new TH1D("h1stjetpt","h1stjetpt",200,0,2000);
-      htrackpt1stjetpt  = new TH2D("htrackpt1stjetpt","htrackpt1stjetpt",100,0,1000,200,0,2000);
-      htrackPtDeDxHarm2 = new TH2D("htrackPtDeDxHarm2","htrackPtDeDxHarm2",100,0,1000,100,0,50);
-      htrackPtCaloIso   = new TH2D("htrackPtCaloIso","htrackPtCaloIso",100,0,1000,100,0,100);
+      hnPFJetsub         = new TH1D("hnPFJetsub","hnPFJetsub",20,0,20);
+      hDeltaPhi          = new TH1D("hDeltaPhi","hDeltaPhi",32,0,3.2);
+      hDeltaPhiMax       = new TH1D("hDeltaPhiMax","hDeltaPhiMax",32,0,3.2);
+      h1stjetpt          = new TH1D("h1stjetpt","h1stjetpt",200,0,2000);
+
+      htrackpt1stjetpt   = new TH2D("htrackpt1stjetpt","htrackpt1stjetpt",100,0,1000,200,0,2000);
+      htrackPtDeDxHarm2  = new TH2D("htrackPtDeDxHarm2","htrackPtDeDxHarm2",100,0,1000,100,0,50);
+      htrackPtASmi       = new TH2D("htrackPtASmi","htrackPtASmi",50,0,500,10,0,1);
+      htrackPtCaloIso    = new TH2D("htrackPtCaloIso","htrackPtCaloIso",50,0,500,20,0,100);
       htrackPtNLostOuter = new TH2D("htrackPtNLostOuter","htrackPtNLostOuter",100,0,1000,20,0,20);
-      hMet          = new TH1D("hMet","hMet",150,0,1500);
+      hMet               = new TH1D("hMet","hMet",150,0,1500);
 
       hgenPtChi    = new TH1D("hgenPtChi","hgenPtChi",300,0,3000);
       hgenPChi     = new TH1D("hgenPChi","hgenPChi",300,0,3000);
@@ -144,9 +158,22 @@ class Hist
     for(unsigned int i=0; i<inputCollection.size(); i++){
       TVector3 trackVector;
       trackVector.SetPtEtaPhi(inputCollection[i].pt,inputCollection[i].eta,inputCollection[i].phi);
-      double p = std::sqrt(std::pow(inputCollection[i].pt,2) + std::pow(trackVector.Pz(),2));
-      double ASmi   = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_pixel,1);
-      double ASmiNP = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_strip,0);
+
+      double p        = std::sqrt(std::pow(inputCollection[i].pt,2) + std::pow(trackVector.Pz(),2));
+      double ASmi     = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_pixel,1);
+      double ASmi_3   = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_pixel,1,3);
+      double ASmi_7   = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_pixel,1,7);
+      double ASmiNP   = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_strip,0);
+      double ASmiNP_3 = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_strip,0,3);
+      double ASmiNP_7 = dEdxOnTheFly(&(*HitsDeDx)[i], &(*HitsShapetest)[i], &(*HitsPathlength)[i], &(*HitsSubdetid)[i], 1, template_strip, template_strip,0,7);
+
+      double _dvx = inputCollection[i].vx - Vertex[0].x;
+      double _dvy = inputCollection[i].vy - Vertex[0].y;
+      double d0   = ( - _dvx*inputCollection[i].py + _dvy*inputCollection[i].px )/inputCollection[i].pt;
+      double _dvz = inputCollection[i].vz - Vertex[0].z;
+      double dZ   = _dvz - ( _dvx*inputCollection[i].px + _dvy*inputCollection[i].py)/inputCollection[i].pt * (inputCollection[i].pz/inputCollection[i].pt);
+    
+    
       htrackP                  ->Fill(p, weight);
       htrackPt                 ->Fill(inputCollection[i].pt, weight);
       
@@ -159,13 +186,20 @@ class Hist
       htrackNLostOuter         ->Fill(inputCollection[i].trackerExpectedHitsOuter_numberOfHits, weight);
       htrackDeDxHarm2          ->Fill(inputCollection[i].dEdxHarm2, weight);
       htrackASmi               ->Fill(ASmi, weight);
+      htrackASmi_3             ->Fill(ASmi_3, weight);
+      htrackASmi_7             ->Fill(ASmi_7, weight);
       htrackASmiNP             ->Fill(ASmiNP, weight);
+      htrackASmiNP_3           ->Fill(ASmiNP_3, weight);
+      htrackASmiNP_7           ->Fill(ASmiNP_7, weight);
+      htrackHighPurity         ->Fill(inputCollection[i].trackHighPurity, weight);
       htrackPtDeDxHarm2        ->Fill(inputCollection[i].pt,inputCollection[i].dEdxHarm2, weight);
+      htrackPtASmi             ->Fill(inputCollection[i].pt,ASmi, weight);
       htrackPtCaloIso          ->Fill(inputCollection[i].pt,trackCaloIsolation(&inputCollection[i]), weight);
       htrackPtNLostOuter       ->Fill(inputCollection[i].pt,inputCollection[i].trackerExpectedHitsOuter_numberOfHits, weight);
 
-      //htrackd0->Fill(inputCollection[i].d0, weight);
-      //htrackdz->Fill(inputCollection[i].dz, weight);
+      htrackd0                 ->Fill(d0, weight);
+      htrackdz                 ->Fill(dZ, weight);
+
       //htrackPMass-> Fill(p,mass, weight);
       //double mass =  sqrt((inputCollection[i].dEdxHitsNPHarm2_1000 - C)*pow(p,2)/K, weight);
       //double mass_7Hits =  sqrt((inputCollection[i].dEdxHitsNPHarm2_7 - C)*pow(p,2)/K, weight);
