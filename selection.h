e46297014b8de067b93255bc1727783becae7260
Teresa Lenz <teresa.lenz@desy.de>
2015-08-03 18:40:09 +0200

    MET>0

--- selection.h
+++ selection.h
@@ -114,7 +114,7 @@ int Event::Selection()
     countsEventCuts->Fill("triggerCut_OnlyData", weight);
 	     
     // 2.) MET cut
-    if(MET_pt<=100.) return 0;
+    if(MET_pt<=0.) return 0;
     countsEventCuts->Fill("metCut", weight);
     
     // 3.) Leading Jet Cut
